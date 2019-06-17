75a39b6faf39bca9c746621e5b109954a67b1902
Davide Benvegnu <davide.benvegnu@outlook.com>
2019-06-17 11:53:35 +0800

    initial

---
+++ main.js
@@ -0,0 +1,12 @@
+// load the http module
+var http = require('http');
+
+// configure our HTTP server
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.end("<h1>Hello Developer Camp! :)</h1>\n");
+});
+
+// listen on localhost:8000
+server.listen(8000);
+console.log("Server listening at http://127.0.0.1:8000/");
