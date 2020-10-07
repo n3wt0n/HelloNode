46700700b3b19bfcfbc97c92ddee5070b5aea4f2
Davide Benvegnù <davide.benvegnu@outlook.com>
2020-10-07 15:01:43 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.end("<h1>Hello Microsoft Build 2020! :)</h1>\n");
+  response.end("<h1>Hello YouTube! :)</h1>\n");
 });
 
-// listen on localhost:8000
-server.listen(8000);
-console.log("Server listening at http://127.0.0.1:8000/");
+// listen on localhost:2345
+server.listen(2345);
+console.log("Server listening at http://127.0.0.1:2345/");
