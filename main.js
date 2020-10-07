8d182a84792e86a8358b1dbd61911155903cab40
Davide Benvegnù <davide.benvegnu@outlook.com>
2020-10-07 07:22:56 +0000

    Changed files

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.end("<h1>Hello YouTube! :)</h1>\n");
+  response.end("<h1>Hello YouTube from Docker! :)</h1>\n");
 });
 
 // listen on localhost:2345
 server.listen(2345);
 console.log("Server listening at http://127.0.0.1:2345/");
+
