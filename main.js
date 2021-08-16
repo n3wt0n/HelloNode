d6897a6ece6c18d827b0295a9d26da44d83d4434
Davide Benvegnù <davide.benvegnu@outlook.com>
2021-08-16 06:00:34 +0000

    Added new url management

--- main.js
+++ main.js
@@ -3,8 +3,15 @@ var http = require('http');
 
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.end("<h1>Hello :)</h1>\n");
+
+  if (request.url === "/new") {
+    response.writeHead(200, {"Content-Type": "text/html"});  
+    response.end("<h1>Hello From CoderDave :)</h1>\n");
+  }
+  else {
+    response.writeHead(200, {"Content-Type": "text/html"});  
+    response.end("<h1>Hello YouTube :)</h1>\n");
+  }
 });
 
 // listen on localhost:2345
